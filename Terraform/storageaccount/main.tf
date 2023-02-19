ee60a51c83a06134394cfff68d9f06aa710c67f7
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-19 23:17:58 +0100

    Update main.tf

--- Terraform/storageaccount/main.tf
+++ Terraform/storageaccount/main.tf
@@ -13,15 +13,10 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "Test-RG" {
-  name     = "Test-RG"
-  location = "West Europe"
-}
-
 resource "azurerm_storage_account" "testmrphead626" {
   name                     = var.storageaccountName
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = "Test-RG"
+  location                 = "West Europe"
   account_tier             = "Standard"
   account_replication_type = "GRS" 
   min_tls_version          = "TLS1_2"
