c632f9ba81b8f8af281c305fa24122313f5537a5
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-19 23:06:22 +0100

    Update main.tf

--- Terraform/storageaccount/main.tf
+++ Terraform/storageaccount/main.tf
@@ -16,3 +16,10 @@ resource "azurerm_storage_account" "testmrphead626" {
   account_replication_type = "GRS" 
   min_tls_version          = "TLS1_2"
 }
+
+resource "azurerm_storage_account_network_rules" "rules" {
+  storage_account_id = azurerm_storage_account.testmrphead626.id
+
+  default_action             = "Deny"
+  bypass                     = ["AzureServices"]
+}
