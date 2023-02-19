d43ad846f7d0c50caa183d26e809d945f2f530b1
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-02-19 22:51:53 +0100

    Update main.tf

--- Terraform/storageaccount/main.tf
+++ Terraform/storageaccount/main.tf
@@ -1,10 +1,15 @@
+provider "azurerm" {
+  version = "=3.44.1"
+  features {} #This is required for v2 of the provider even if empty or plan will fail
+}
+
 resource "azurerm_resource_group" "Test-RG" {
   name     = "Test-RG"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "testmrphead626" {
-  name                     = "testmrphead626"
+  name                     = var.storageaccountName
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
