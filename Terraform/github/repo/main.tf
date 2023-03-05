bab79e5486e0dd5a49a0fa10c9a4a85ac8519f26
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-05 19:04:32 +0100

    Update main.tf

--- Terraform/github/repo/main.tf
+++ Terraform/github/repo/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.44.1"
+    }
+  }
 
+  required_version = ">= 1.3.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "testmrphead626" {
+  name                     = var.storageaccountName
+}
