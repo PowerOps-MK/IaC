04f0951415a66ae3b953cf2b6035ead0470d8a91
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-05 22:09:15 +0100

    Update variables.tf

--- Terraform/github/repo/variables.tf
+++ Terraform/github/repo/variables.tf
@@ -1,7 +1,7 @@
 variable "repo_name" {
   description = "The Name of the repository"
   type        = string
-  default     = "Test Terraform Repo"
+  default     = "Test-terraform"
 }
 
 variable "token" {
