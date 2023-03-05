6a69ee9887e4afa1f2c23e3f97f41e1f61395c1c
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-05 22:29:56 +0100

    Update variables.tf

--- Terraform/github/repo/variables.tf
+++ Terraform/github/repo/variables.tf
@@ -1,12 +1,18 @@
+variable "token" {
+  description = "The token for Github"
+  type        = string
+  sensitive   = true
+  default     = "XX_TOKEN"
+}
+
 variable "repo_name" {
   description = "The Name of the repository"
   type        = string
   default     = "Test-terraform"
 }
 
-variable "token" {
-  description = "The token for Github"
+variable "branch_name" {
+  description = "The Name of the default branch"
   type        = string
-  sensitive   = true
-  default     = "XX_TOKEN"
+  default     = "master"
 }
