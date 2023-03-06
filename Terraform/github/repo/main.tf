5abed6581e6f8d126656197923c11c4b76285cc4
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-03-06 20:12:06 +0100

    Update main.tf

--- Terraform/github/repo/main.tf
+++ Terraform/github/repo/main.tf
@@ -14,7 +14,7 @@ provider "github" {
 resource "github_repository" "repo" {
   name               = var.repo_name
   description        = "A private-visible repository created by Terraform"
-  visibility         = "private"
+  visibility         = "public" #"private"
   has_issues         = true
   auto_init          = true
   license_template   = "lgpl-3.0"
